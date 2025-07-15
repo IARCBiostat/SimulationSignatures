5b3cf452a24f02f97f8d77c3aaf4b359639219df
Diana Wu <dianawu3214@hotmail.com>
2025-07-15 18:03:12 +0200

    Update scenario_2.R

--- config/scenario_2.R
+++ config/scenario_2.R
@@ -9,11 +9,3 @@ BETAS  <- c(0.1, 0.2, 0.3, 0.4, 0.5)
 rhoM   <- c(0)
 SecondLayerM <- TRUE
 
-# NCore <- 40
-# pM    <- 1000
-# NSIM  <- 1:30
-# N     <- c(1000)
-# PMRELS <- c(5, 25, 125)
-# BETAS  <- c(0.1, 0.2, 0.3, 0.4, 0.5)
-# rhoM   <- c(0)
-# SecondLayerM <- TRUE
